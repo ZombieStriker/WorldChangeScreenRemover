4a78af9e6e7c8470de4bfd03f607745c2a67f298
Zombie_Striker <dannyboyhn@yahoo.com>
2022-01-06 22:17:27 -0500

    1.0.19

--- src/me/zombie_striker/wcsr/Main.java
+++ src/me/zombie_striker/wcsr/Main.java
@@ -3,7 +3,6 @@ package me.zombie_striker.wcsr;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.zombie_striker.wcsr.DependencyDownloader.ProjectID;
 import me.zombie_striker.wcsr.placeholderapi.WCSR_PHE;
 import net.md_5.bungee.api.ChatColor;
 
@@ -33,31 +32,8 @@ public class Main extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		try {
-			// if (Bukkit.getPluginManager().getPlugin("PluginConstructorAPI") == null)
-			// GithubDependDownloader.autoUpdate(this, new
-			// File(getDataFolder().getParentFile(),"PluginConstructorAPI.jar"),
-			// "ZombieStriker", "PluginConstructorAPI", "PluginConstructorAPI.jar");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		try {
-			if (!getConfig().contains("autoUpdate")) {
-				getConfig().set("autoUpdate", true);
-				saveConfig();
-			}
-			if (getConfig().getBoolean("autoUpdate"))
-				GithubUpdater.autoUpdate(this, "ZombieStriker", "WorldChangeScreenRemover",
-						"WorldChangeScreenRemover.jar");
-			// No longer using the github updater. The updater below uses BukkitDev to get
-			// the update.
-			// new Updater(this,289776);
-		} catch (Error | Exception e) {
-
-		}
 		if (!getServer().getPluginManager().isPluginEnabled("ProtocolLib")) {
-			Bukkit.broadcastMessage("[WorldChangeScreenRemover] ProtocolLib has not been installed. Installing now");
-			new DependencyDownloader(this, ProjectID.PROTOCOLLIB);
+			return;
 		}
 		updateStuff();
 		getCommand("wcsr").setExecutor(new WCSRCommand(this));
